0b3446d6c1de661f3b16e7aa16aecf24f3724196
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-01-31 20:08:13 -0500

    error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Menu from "../components/Menu";
+import MyOrder from "../container/MyOrder";
 import menu from "../assets/icons/icon_menu.svg";
+import AppContext from "../context/AppContext";
 import shopingCart from "../assets/icons/icon_shopping_cart.svg";
 import logo from "../assets/logos/logo_yard_sale.svg";
 import "../styles/Header.css";
+import "../styles/MyOrder.css";
 
 function Header() {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
+  const { state } = useContext(AppContext);
   const handleToggle = () => {
     setToggle(!toggle);
   };
+
   return (
     <nav>
-      <img src={menu} alt="menu" className="menu" />
+      <img
+        src={menu}
+        alt="menu"
+        className="menu
+      "
+      />
       <div className="navbar-left">
         <img src={logo} alt="logo" className="nav-logo" />
         <ul>
@@ -41,13 +52,17 @@ function Header() {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li
+            className="navbar-shopping-cart"
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shopingCart} alt="shopping cart" />
-            <div>2</div>
+            {state.cart.length > 0 ? <div> {state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 }
--- src/container/MyOrder.jsx
+++ src/container/MyOrder.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-import OrderItem from "@components/OrderItem";
-// import "@styles/MyOrder.scss";
-import arrow from "@icons/flechita.svg";
+import { useContext } from "react";
+import AppContext from "../context/AppContext.jsx";
+import OrderItem from "../components/OrderItem";
+import "../styles/MyOrder.css";
+import "../styles/OrderItem.css";
+import arrow from "../assets/icons/flechita.svg";
 
 const MyOrder = () => {
+  const { state } = useContext(AppContext);
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -11,12 +20,15 @@ const MyOrder = () => {
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        <OrderItem />
+        {state.cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        ))}
+
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
